73adff40231d4eba7d7360ec3914e61fb278724d
dbarai <dulalbarai01@gmail.com>
2026-01-30 02:58:01 +0530

    Add index.php for e-commerce project setup

---
+++ htdocs/ecommerce/index.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-Commerce Project</title>
+</head>
+<body>
+<h1>Project Setup Successful 🚀</h1>
+<p>Database Connected</p>
+</body>
+</html>
